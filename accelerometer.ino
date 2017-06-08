5133fdfd5bcdc34275cfcd58e87e26495d5d69b1
Suraj K <SurajIyyengar@users.noreply.github.com>
2017-06-08 14:53:36 +0530

    Accelerometer Code added
    Obtain the values of ax, ayand az.

---
+++ accelerometer.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(A0, INPUT);
+pinMode(A1, INPUT);
+pinMode(A2, INPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.print("X=");
+Serial.print(analogRead(A0));
+Serial.print("\tY=");
+Serial.print(analogRead(A1));
+Serial.print("\tZ=");
+Serial.println(analogRead(A2));
+
+}
