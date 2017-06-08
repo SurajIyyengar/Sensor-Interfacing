365b140c35e3ab67a3bcd4dbf36fa05f1b407b0a
Suraj K <SurajIyyengar@users.noreply.github.com>
2017-06-08 15:21:52 +0530

    GPS-GSM via Serial Monitor
    Communicating with GPS-GSM module via serial monitor using AT commands

---
+++ GSM_GPS_Serial.ino
@@ -0,0 +1,21 @@
+//arduino mega 2560 and gsm-gps sim 808
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial1.begin(115200);
+ Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+while(Serial.available())
+{
+  Serial1.write(Serial.read());
+}
+while(Serial1.available())
+{
+  Serial.print((char)Serial1.read());
+  //Serial.write(Serial1.read());
+}
+}
