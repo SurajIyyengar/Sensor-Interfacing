e233b59385e8a8e123d63642b1dd712ff808bd28
Suraj K <SurajIyyengar@users.noreply.github.com>
2017-06-08 15:12:42 +0530

    SONAR
    Returns the distance of an object with in a range of 0-400m

---
+++ sonar.ino
@@ -0,0 +1,36 @@
+
+//HC SR04 with arduino uno
+
+int trig=2;
+int duration,distance;
+int echo=3;
+//As soon as the SONAR is triggered by the arduino the SONAR sends a 8 cycle SONIC burst after at the end of the echo is turned on. The echo goes low when the
+//reflected sound is received at the SONAR
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(trig,LOW);
+delay(5);
+
+digitalWrite(trig,HIGH);
+delay(1000);
+digitalWrite(trig,LOW);
+
+duration = pulseIn(echo, HIGH);
+//Serial.println(duration);
+distance = duration*0.034/2;
+if (distance >= 400|| distance <= 0){
+ Serial.println("-1");
+ }
+ else {
+ Serial.println(distance);
+ }
+
+delay(50);
+}
